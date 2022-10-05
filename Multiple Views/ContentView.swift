a22d84cc58a6097b16b62af34cc45f3e3f200b83
jmadison <jmad99@sbcglobal.net>
2022-10-05 09:27:54 -0500

    Initial Commit

---
+++ Multiple Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Multiple Views
+//
+//  Created by Josh Madison on 10/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Multiple Views/Multiple_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiple_ViewsApp.swift
+//  Multiple Views
+//
+//  Created by Josh Madison on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Multiple_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
